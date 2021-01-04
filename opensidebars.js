52edc45421fa863870dfe0020af8888bf5091b0f
Abhay Prasanna <67134545+abhayprasanna@users.noreply.github.com>
2021-01-03 20:29:41 -0500

    Create opensidebars.js

---
+++ opensidebars.js
@@ -0,0 +1,23 @@
+const rightsidebar = new KeyboardEvent("keydown", {
+  bubbles: true, cancelable: true, keyCode: 220, ctrlKey: true, shiftKey: true
+  });
+const rightsidebarMac = new KeyboardEvent("keydown", {
+  bubbles: true, cancelable: true, keyCode: 220, metaKey: true, shiftKey: true
+  });
+const leftsidebar = new KeyboardEvent("keydown", {
+  bubbles: true, cancelable: true, keyCode: 220, ctrlKey: true, shiftKey: false
+  });
+const leftsidebarMac = new KeyboardEvent("keydown", {
+  bubbles: true, cancelable: true, keyCode: 220, metaKey: true, shiftKey: false
+  });
+
+if (typeof firstRun == 'undefined') {
+  document.dispatchEvent(rightsidebar);
+  document.dispatchEvent(rightsidebarMac);
+  if (document.getElementsByClassName("bp3-button bp3-minimal bp3-icon-menu-closed").length == 0) {
+  document.dispatchEvent(leftsidebar);
+  document.dispatchEvent(leftsidebarMac);
+  }
+  console.log('Both sidebars open!');
+  window.firstRun = false;
+}
