051c92b7e3c3407a5028e01b3c0ef1f8810599f1
Abhay Prasanna <67134545+abhayprasanna@users.noreply.github.com>
2021-01-30 11:54:11 -0500

    Update opensidebars.js

--- opensidebars.js
+++ opensidebars.js
@@ -1,5 +1,15 @@
+//Set delay to trigger sidebars
 setTimeout(loadToggleSidebars, 1000);
-async function loadToggleSidebars() {
+//Set delay to scroll to bottom of daily notes
+setTimeout(ScrollToBottom, 2500);
+
+//Define function to find first reference section and scroll it into view
+function ScrollToBottom() {
+ document.querySelector(".rm-reference-main").scrollIntoView();
+}
+
+//Define function to toggle sidebars using Roam default keyboard shortcuts
+function loadToggleSidebars() {
   const rightsidebar = new KeyboardEvent("keydown", {
   bubbles: true, cancelable: true, keyCode: 220, ctrlKey: true, shiftKey: true
   });
@@ -13,9 +23,11 @@ const leftsidebarMac = new KeyboardEvent("keydown", {
   bubbles: true, cancelable: true, keyCode: 220, metaKey: true, shiftKey: false
   });
 
+//Check if first run and only run once
 if (typeof firstRun == 'undefined') {
   document.dispatchEvent(rightsidebar);
   document.dispatchEvent(rightsidebarMac);
+//Check if left sidebar is already open
   if (document.getElementsByClassName("bp3-button bp3-minimal bp3-icon-menu-closed").length == 0) {
   document.dispatchEvent(leftsidebar);
   document.dispatchEvent(leftsidebarMac);
