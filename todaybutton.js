0b71bfd600b7e3c146fead2a9d826fee3709a9a9
Abhay Prasanna <67134545+abhayprasanna@users.noreply.github.com>
2021-02-06 15:44:26 -0500

    Create todaybutton.js

---
+++ todaybutton.js
@@ -0,0 +1,29 @@
+setTimeout(todaybutton, 500); //wait half a second just in case
+
+function todaybutton() { //creates a new left sidebar log button below Daily Notes
+  if (!document.querySelector('bp3-icon bp3-icon-timeline-events icon')) {
+    var divOne = document.createElement('div');
+    divOne.classList.add('log-button');
+    divOne.innerHTML = "TODAY";
+    var spanTwo = document.createElement('span');
+    spanTwo.classList.add('bp3-icon', 'bp3-icon-timeline-events', 'icon');
+    divOne.prepend(spanTwo);
+    var sidebarcontent = document.querySelector("#app > div.roam-body > div.roam-app > div.roam-sidebar-container.noselect > div"),
+        sidebartoprow = sidebarcontent.childNodes[1];
+    if (sidebarcontent && sidebartoprow) {
+      sidebartoprow.parentNode.insertBefore(divOne, sidebartoprow.nextSibling);    
+    }
+    divOne.onclick = toggletoday; //run the function onclick
+  }
+}
+
+function toggletoday() { //creates the URL for today and opens it
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = today.getFullYear();
+  today = mm + '-' + dd + '-' + yyyy;
+  let roamuri = "https://roamresearch.com/#/app/abhay/page/";
+  let roamuritoday = roamuri+today;
+  window.open(roamuritoday,"_self");
+}
