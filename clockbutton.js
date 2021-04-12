a8694a51b57a260875fb0815a19925799c7053f6
Abhay Prasanna <67134545+abhayprasanna@users.noreply.github.com>
2021-04-12 15:26:15 -0400

    Update clockbutton.js

--- clockbutton.js
+++ clockbutton.js
@@ -1,6 +1,8 @@
-setTimeout(timebutton, 700); //wait half a second just in case
+// Creates a button on left sidebar that displays current time, and takes you to the bottom of Today's Daily Note on-click
 
-function timebutton() { //creates a new left sidebar log button
+setTimeout(timebutton, 500); //wait half a second just in case
+
+function timebutton() { //creates a new left sidebar log button below Daily Notes
   if (!document.getElementById('timeDiv')) {
     var divTime = document.createElement('div');
     divTime.classList.add('log-button');
@@ -12,9 +14,29 @@ function timebutton() { //creates a new left sidebar log button
     if (sidebarcontent && sidebartoprow) {
       sidebartoprow.parentNode.insertBefore(divTime, sidebartoprow.nextSibling);    
     }
+    divTime.onclick = toggletoday; //run the function onclick
   }
   setInterval(function() {
-    divTime.innerHTML = new Date().toLocaleTimeString(); //adds time
-    divTime.prepend(spanTime); //adds clock icon
+    divTime.innerHTML = new Date().toLocaleTimeString();
+    divTime.prepend(spanTime);
   }, 1);
+}
+
+//Define function to find reference section and scroll it into view
+function ScrollToBottom() {
+ document.querySelector(".rm-reference-main").scrollIntoView();
+ console.log('Scrolled to bottom!');
+}
+
+function toggletoday() { //creates the URL for today and opens it
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = today.getFullYear();
+  today = mm + '-' + dd + '-' + yyyy;
+  dbname = window.location.href.split('/')[5];
+  let roamuri = "https://roamresearch.com/#/app/";
+  let roamuritoday = roamuri+ dbname + "/page/" + today;
+  window.open(roamuritoday,"_self");
+  setTimeout(ScrollToBottom, 300);  //Set delay to scroll to bottom of daily note
 }
