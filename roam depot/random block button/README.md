b92b23e16b28786b6f102d8f1c12c3b270adc923
Abhay Prasanna <albatross86@gmail.com>
2022-07-11 14:18:52 -0400

    Roam depot first extension

---
+++ roam depot/random block button/README.md
@@ -0,0 +1,5 @@
+# Random Block Button ("I'm Feeling Lucky!")
+
+## Use Case
+Adds a button to the left sidebar called "Random Block". On click, this will open a random block in your graph.
+This is a simple and easy way to bring some serendipity and spaced repetition into your use of Roam Research.
---
+++ roam depot/random block button/extension.js
@@ -0,0 +1,26 @@
+setTimeout(randombutton, 400); //wait half a second just in case
+
+function randombutton() { //creates a new left sidebar log button below Daily Notes
+  if (!document.getElementById('randomDiv')) {
+    var divRandom = document.createElement('div');
+    divRandom.classList.add('log-button');
+    divRandom.innerHTML = "Random Block";
+    divRandom.id = 'randomDiv';
+    var spanRandom = document.createElement('span');
+    spanRandom.classList.add('bp3-icon', 'bp3-icon-random', 'icon');
+    divRandom.prepend(spanRandom);
+    var sidebarcontent = document.querySelector("#app > div.roam-body > div.roam-app > div.roam-sidebar-container.noselect > div"),
+        sidebartoprow = sidebarcontent.childNodes[1];
+    if (sidebarcontent && sidebartoprow) {
+      sidebartoprow.parentNode.insertBefore(divRandom, sidebartoprow.nextSibling);    
+    }
+    divRandom.onclick = togglerandom; //run the function onclick
+  }
+}
+
+function togglerandom() { //creates the URL for random page or block and opens it
+  random = window.roamAlphaAPI.q('[:find [(rand 1 ?block-uid)] :where [?e :block/page] [?e :block/uid ?block-uid] ]')[0];
+  window.roamAlphaAPI.ui.mainWindow
+    .openBlock({block:
+				{uid: random[0]}});
+}
